2b51de3bf0354a6f24529930f3fff14d3bf32469
Esrivn <software@esrivn.com>
2023-03-23 17:33:39 +0700

    uppdate

--- testTranslations/src/runtime/translations/default.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    field1: 'field 1',
-    field2: 'field 2',
-    field3: 'field3'
-}
--- testTranslationsFunction/src/runtime/translations/vi.js
+++
@@ -1,5 +0,0 @@
-define({
-    field1: 'Loại lô đất',
-    field2: 'Trường 2',
-    field3: 'Trường 3'
-});
